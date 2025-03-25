35338cbff1c4cb867956032cb7c01e3d87b6798f
xingchen4726 <3084622594@qq.com>
2025-03-25 20:47:15 +0800

    更新背景图片的大小适应及唤醒界面美化

--- custom/content.js
+++ custom/content.js
@@ -1,18 +1,22 @@
 chrome.storage.local.get(['backgroundUrl'], (result) => {
   if (result.backgroundUrl) {
-    document.body.style.backgroundImage = `url(${result.backgroundUrl})`;
-    document.body.style.backgroundSize = 'cover';
-    document.body.style.backgroundPosition = 'center';
+    setFixedBackground(result.backgroundUrl);
   }
 });
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "updateBackground") {
     chrome.storage.local.get(['backgroundUrl'], (result) => {
       if (result.backgroundUrl) {
-        document.body.style.backgroundImage = `url(${result.backgroundUrl})`;
-        document.body.style.backgroundSize = 'cover';
-        document.body.style.backgroundPosition = 'center';
+        setFixedBackground(result.backgroundUrl);
       }
     });
   }
 });
+
+function setFixedBackground(backgroundUrl) {
+  document.body.style.backgroundImage = `url(${backgroundUrl})`;
+  document.body.style.backgroundSize = 'cover'; // 背景图覆盖整个窗口
+  document.body.style.backgroundPosition = 'center'; // 背景图居中
+  document.body.style.backgroundAttachment = 'fixed'; // 背景图固定，不随滚动条移动
+}
